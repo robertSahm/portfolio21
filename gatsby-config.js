8b2695f77a8fcef9a83f7e2c507fe711324dd301
robertSahm <robertsahm@gmail.com>
2021-06-01 14:43:16 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "portfolio21",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
